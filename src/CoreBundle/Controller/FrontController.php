6c4986d78452c1c151f4c8fe0dd7da7bb00faf13
morad <vdeale85@gmail.com>
2017-08-25 19:05:32 +0200

    Test autocompletion

--- src/CoreBundle/Controller/FrontController.php
+++ src/CoreBundle/Controller/FrontController.php
@@ -3,11 +3,39 @@
 namespace CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\ORM\EntityRepository;
 
 class FrontController extends Controller
 {
     public function indexAction()
     {
         return $this->render('CoreBundle:Front:index.html.twig');
+    }
+
+    public function ajaxnatureAction(Request $request)
+    {
+       //$request = $this->get('request');
+        $response = new Response;
+         
+        if($request->isXmlHttpRequest())
+        {
+            $term = $this->get('motcle');
+             
+            $array= $this->getDoctrine()
+                ->getManager()
+                ->getRepository('CoreBundle:Espece')
+                ->listeNature($term);
+             
+            $response = new Response(json_encode($array));
+             
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;      
+        }
+        else {
+            return $this->render('CoreBundle:Front:testAUto.html.twig');
+        }
     }
 }
--- src/CoreBundle/Repository/EspeceRepository.php
+++ src/CoreBundle/Repository/EspeceRepository.php
@@ -3,7 +3,29 @@
 namespace CoreBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
-class EspeceRepository extends EntityRepository
+class EspeceRepository extends \Doctrine\ORM\EntityRepository
 {
+	public function listeNature($term)
+    {
+        $qb = $this->createQueryBuilder('c');
+         
+        $qb ->select('c.nomVern')
+            
+            ->where('c.nomVern LIKE :term')
+            ->setParameter('term', '%'.$term.'%');
+         
+        $arrayAss= $qb->getQuery()
+                   ->getArrayResult();
+         
+        // Transformer le tableau associatif en un tableau standard
+        $array = array();
+        foreach($arrayAss as $data)
+        {
+            $array[] = $data['nomVern'];
+        }
+     
+        return $array;
+    }
 }
