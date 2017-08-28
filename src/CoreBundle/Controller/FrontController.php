b7a01aba09552a32f41b4e4bb4df4fc105160f5f
jeapmamy <jeap.mamy@gmail.com>
2017-08-28 16:39:11 +0200

    Utilisateurs USER et ADMIN

--- src/CoreBundle/Controller/FrontController.php
+++ src/CoreBundle/Controller/FrontController.php
@@ -3,11 +3,20 @@
 namespace CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class FrontController extends Controller
 {
     public function indexAction()
     {
         return $this->render('CoreBundle:Front:index.html.twig');
+    }
+	
+	/**
+	 *@Security("has_role('ROLE_ADMIN')")
+	 */
+	public function adminAction()
+    {		
+		return $this->render('CoreBundle:Front:admin.html.twig');
     }
 }
