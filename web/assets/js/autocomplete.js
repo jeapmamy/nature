d74c016d84f1c09c076cc650b99e86d9273935ec
morad <vdeale85@gmail.com>
2017-09-04 15:45:34 +0200

    (carte)

--- web/assets/js/autocomplete.js
+++ web/assets/js/autocomplete.js
@@ -1,10 +1,4 @@
-var self = this;
-function initMap(){
-        self.map = new google.maps.Map($('#map')[0], {
-        center: new google.maps.LatLng(46.606111,1.845278),
-        zoom: 5,
-    });
-}
+
 
 $("input[data-id=listedesespeces]").autocomplete({
     source: function (request, response) {
@@ -80,4 +74,3 @@ $("input[data-id=listedesespeces]").on('autocompleteselect',function(event, ui) 
     })
 
 })
-
